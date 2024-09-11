6e245626f5686e5f7f1c0d65911c18b4649da496
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-12 01:43:30 +0300

    Frontend changes

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -1,12 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div class="container shopping-cart">
+    <div class="content">
+      <div class="row">
+        <div class="col-md-12 col-lg-12" v-for='(item, index) in products' :key='index'>
+          <div class="items">
+            <div class="product">
+              <div class="row">
+                <div class="col-md-3">
+                  <img class="img-fluid mx-auto d-block image" :src="getImageUrl(item.image)">
+                </div>
+                <div class="col-md-6">
+                  <div class="info">
+                    <div class="row">
+                      <div class="col-md-5 product-name">
+                        <div class="product-name">
+                          <a class="product-name" v-bind:href="'/detail/' + item.id">{{ item.name }}</a>
+                          <div class="product-info">
+                            <div class="product-info-detail">{{ item.description }}</div>
+                            <div class="product-info-detail">Seller: <a class="value" v-bind:href="item.seller.id">{{
+                                item.seller.name
+                              }}</a>
+                            </div>
+                            <div class="product-info-detail"><span v-if="item.freeDelivery"> FREE </span>Delivery: <span
+                                class="value">{{ item.deliveryIn }}</span></div>
+                            <div class="product-info-detail"><span class="value" v-html="item.features"></span></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-3">
+                  <div class="row info">
+                    <div class="col-md-6 quantity">
+                      <label for="quantity">Quantity:</label>
+                      <input id="quantity" type="number" value="1" class="form-control quantity-input">
+                    </div>
+                    <div class="col-md-6 price">
+                      <span>{{ item.money }} {{ item.price }}  {{ item.moneySymbol }}</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-8"></div>
+                <div class="col-md-4">
+                  <div class=""></div>
+                  <div class="row align-items-end">
+                    <div class="col-md-6">
+                      <button class="btn btn-primary">Add to Basket</button>
+                    </div>
+                    <div class="col-md-6">
+                      <button class="btn btn-success">Buy now</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
