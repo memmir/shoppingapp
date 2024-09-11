c99d29055e9dd23266fb8cbed2c4a183b655514f
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-11 18:04:57 +0300

    Initial commit

---
+++ src/main/java/com/ater/shoppingapp/ShoppingappApplication.java
@@ -0,0 +1,13 @@
+package com.ater.shoppingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingappApplication.class, args);
+	}
+
+}
