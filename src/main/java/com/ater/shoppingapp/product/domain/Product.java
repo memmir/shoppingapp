d8e53c25c0c21adb641509446fd1364d39f37840
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-13 11:41:39 +0300

    Added MongoDB collection mapper annotation and required.

--- src/main/java/com/ater/shoppingapp/product/domain/Product.java
+++ src/main/java/com/ater/shoppingapp/product/domain/Product.java
@@ -1,9 +1,27 @@
 package com.ater.shoppingapp.product.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document(collection = "product")
+//Oluşturdğumuz modelin MongoDb de bulunan collection lara karşılık gelmesini sağlayan annotation.
+// Spring Data daki gibi entity kullanıyorduk relational databaselerde. Mongodb de document kullanılıyor.
+//Mongo relational database olmadığı için collectionlar var
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id") // 2 product price nesnesinin birbirine eşit olup olmadığını anlayacağız değişken id değişkeni olduğunu belirttik.
 public class Product {
 
     private String id;
     private String name;
-    private Boolean active;
+    private String description;
+    private String companyId;
+    private String features;
+    private String categoryId;
     private String productCode;
+    private Boolean active;
 }
