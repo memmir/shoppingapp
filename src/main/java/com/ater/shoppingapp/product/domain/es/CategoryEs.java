41015da34a96c677c1648b005215f7a1a67940ef
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-14 02:50:53 +0300

    Updated elastic search classes.

--- src/main/java/com/ater/shoppingapp/product/domain/es/CategoryEs.java
+++ src/main/java/com/ater/shoppingapp/product/domain/es/CategoryEs.java
@@ -1,5 +1,10 @@
 package com.ater.shoppingapp.product.domain.es;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CategoryEs {
 
     private String id;
--- src/main/java/com/ater/shoppingapp/product/domain/es/CompanyEs.java
+++ src/main/java/com/ater/shoppingapp/product/domain/es/CompanyEs.java
@@ -1,5 +1,8 @@
 package com.ater.shoppingapp.product.domain.es;
 
+import lombok.Data;
+
+@Data
 public class CompanyEs {
 
     private String id;
