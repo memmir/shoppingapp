8d97ebe0ad9c26cdebf19b360805445a2eaa1a79
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-13 11:42:13 +0300

    Created ProductPrice model class.

---
+++ src/main/java/com/ater/shoppingapp/product/domain/ProductPrice.java
@@ -0,0 +1,20 @@
+package com.ater.shoppingapp.product.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "product_price")
+//Oluşturdğumuz modelin MongoDb de bulunan collection lara karşılık gelmesini sağlayan annotation.
+// Spring Data daki gibi entity kullanıyorduk relational databaselerde. Mongodb de document kullanılıyor.
+//Mongo relational database olmadığı için collectionlar var
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id") // 2 product price nesnesinin birbirine eşit olup olmadığını anlayacağız değişken id değişkeni olduğunu belirttik.
+public class ProductPrice {
+    private String id;
+    private String productId;
+    private String currency;
+    private String price;
+}
