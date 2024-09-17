c19cf89ee0e0b1246dc9366abdc76907539551b9
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-17 10:47:19 +0300

    Added annotation to obligate the cross Origin error.

--- src/main/java/com/ater/shoppingapp/product/api/ProductApi.java
+++ src/main/java/com/ater/shoppingapp/product/api/ProductApi.java
@@ -3,6 +3,7 @@ package com.ater.shoppingapp.product.api;
 import com.ater.shoppingapp.product.model.ProductResponse;
 import com.ater.shoppingapp.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import reactor.core.publisher.Flux;
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ProductApi {
 
     private final ProductService productService;
